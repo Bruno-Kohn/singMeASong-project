5d9825b3174842549f2693e580fe34090a67c61b
Bruno-Kohn <brunokohn1993@gmail.com>
2021-12-02 10:02:17 -0300

    docs: initial project files

---
+++ src/database.js
@@ -0,0 +1,15 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const config = {
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: '123456',
+  database: 'sing_me_a_song',
+};
+
+const connection = new Pool(config);
+
+export default connection;
